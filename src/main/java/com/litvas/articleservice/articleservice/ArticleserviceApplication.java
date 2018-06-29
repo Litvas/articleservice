50b26db3c204d9c8e49c56813e330235792cbccf
Litvas <kpifs92@yandex.ru>
2018-06-29 21:28:02 +0300

    add frame of progect from start.spring.io

---
+++ src/main/java/com/litvas/articleservice/articleservice/ArticleserviceApplication.java
@@ -0,0 +1,12 @@
+package com.litvas.articleservice.articleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleserviceApplication.class, args);
+	}
+}
